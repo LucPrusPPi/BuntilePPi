2e4aeb648d226a38c76b4cabef70582ebb8fd448
LucPrusPPi <105052037+LucPrusPPi@users.noreply.github.com>
2022-05-22 12:20:47 +0300

    Add files via upload

---
+++ BuntilePPi.py
@@ -0,0 +1,48 @@
+from colorama import init, AnsiToWin32
+from colorama import Fore, Back, Style
+from termcolor import colored
+say = print
+import time
+prefix = Fore.RED + '[BuntilePPi] '
+say(prefix + "Downloading files... 10%")
+import random
+import webbrowser
+from random import randint
+import sys
+pw = "Please wait..."
+time.sleep(2)
+say(prefix + "Downloading files... 34%")
+stream = AnsiToWin32(sys.stderr).stream
+from tkinter import *
+window = Tk
+alert = window
+time.sleep(3)
+import emoji
+import turtle
+t = turtle
+forward = t.forward
+bloom = t.color
+forth = forward
+left = t.left
+right = t.right
+from emoji import *
+r = random
+fgr = Fore
+bgr = Back
+import sys
+init()
+init(autoreset=True)
+import re
+say(prefix + "Downloading files... 68%")
+web = webbrowser
+time.sleep(1)
+reg = web.register
+fromc = input
+youtube = 'https://www.youtube.com/channel/UCFt1dxBYbcCCI6fa0DHNggw'
+download100 = say(prefix + "Downloading files... 100%")
+if re.search(r'\.', youtube):
+    web.open_new_tab(youtube)
+time.sleep(3)
+say(fgr.GREEN + 'Complete!')
+say(fgr.CYAN+'Create by LucPrus.','YouTube: '+youtube)
+say(fgr.LIGHTYELLOW_EX+"Plugin Vershon: 0.2.3")
